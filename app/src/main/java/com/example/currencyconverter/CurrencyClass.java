8e5e8852f30cdac8a3742b721c78953f253af9f0
AndreiSec <andreisecara01@gmail.com>
2021-02-10 19:52:41 -0500

    Update

---
+++ app/src/main/java/com/example/currencyconverter/CurrencyClass.java
@@ -0,0 +1,40 @@
+package com.example.currencyconverter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class CurrencyClass {
+    private static double USDtoCad = 1.27;
+    private static double USDtoEur = 0.83;
+    private static double USDtoJPY = 104.60;
+    private static double USDtoGbp = 0.72;
+    private static double USDtoChf = 0.89;
+
+    private enum CurrencyRates{
+
+    }
+
+    public static String currencyFrom;
+    public static String currencyTo;
+
+    // Converts currencies and outputs the result as a string
+    public static String currencyConvertFunction(){
+    String result = null;
+    Double numberResult = 0.00;
+
+
+
+
+
+
+
+    return result;
+    }
+
+}
--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -13,29 +13,44 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    // CREATE OBJECT FOR NUMBER INPUT FIELD
+    public EditText inputNumber = findViewById(R.id.editTextFromInput);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        // CREATE TO AND FROM CURRENCY PICKER SPINNERS
+        Spinner spinnerFrom = findViewById(R.id.spinnerFrom);
+        Spinner spinnerTo = findViewById(R.id.spinnerTo);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.currencies, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerFrom.setAdapter(adapter);
+        spinnerFrom.setOnItemSelectedListener(this);
+        spinnerTo.setAdapter(adapter);
+        spinnerTo.setOnItemSelectedListener(this);
+        //
 
 
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
@@ -60,16 +75,32 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void cashButtonClickFunction(View view){
 
-        ImageButton image = findViewById(R.id.cashButton);
-//        EditText myTextField = (EditText) findViewById(R.id.cashButton);
-        Log.i("Info", "Cash Button Pressed");
+    // What happens when "convert" button is pressed!!
+    public void convertButtonClickFunction(View view){
 
+    Log.i("CurrencyFrom: ", CurrencyClass.currencyFrom);
+        Log.i("CurrencyTo: ", CurrencyClass.currencyTo);
+        Log.i("Input Value: ", inputNumber.getText().toString());
 
-        image.setImageResource(R.drawable.fl_studio_14089);
-//        Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_LONG).show();
+        // Since all variables are public, this function does not need any parameters.
+        String result = CurrencyClass.currencyConvertFunction();
 
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch(parent.getId()){
+            case R.id.spinnerFrom:
+                CurrencyClass.currencyFrom = parent.getItemAtPosition(position).toString();
+                break;
+            case R.id.spinnerTo:
+                CurrencyClass.currencyTo = parent.getItemAtPosition(position).toString();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
